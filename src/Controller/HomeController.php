4ba3befb1d2565ea8699830e8780d931f5b9c14b
Ad Tou <adel-touat@live.fr>
2024-05-23 14:43:24 +0200

    navbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController
+class HomeController extends AbstractController
 {
+    #[Route('/', 'home.index', methods:['GET'])]
     public function index(): Response
     {
-        
+            return $this->render('home.html.twig');
     }
 }
