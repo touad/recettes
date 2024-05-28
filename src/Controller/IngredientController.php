9d19aaf822a5c4cd50188e4e8d6cc48204faaa3e
Ad Tou <adel-touat@live.fr>
2024-05-29 00:47:24 +0200

    pagination

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,19 +3,26 @@
 namespace App\Controller;
 
 use App\Repository\IngredientRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IngredientController extends AbstractController
 {
     #[Route('/ingredient', name: 'app_ingredient')]
-    public function index(IngredientRepository $repository): Response
+    public function index(PaginatorInterface $paginator,IngredientRepository $repository, Request $request): Response
     {
-        $ingredients = $repository->findAll();
+        $ingredients = $paginator->paginate(
+            $repository->findAll(), 
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
         
         return $this->render('pages/ingredient/index.html.twig', [
-                'ingredients' => $repository->findAll()
+                'ingredients' => $ingredients
         ]);
     }
 }
